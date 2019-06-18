74633f0029cebe8028063ef5d2ab3bfb450ea5ab
William Liu <superliuxz@gmail.com>
2019-06-18 11:52:01 -0700

    prototyped shopping-list component; finished basic set up of the project.

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,10 +1,15 @@
 import {Component, OnInit} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new Ingredient('Chicken', 5),
+    new Ingredient('Garlic', 2)
+  ];
 
   constructor() {
   }

ea6015933cf3ecdda42a026cca6a16353a6835a0
William Liu <superliuxz@gmail.com>
2019-06-18 10:53:50 -0700

    add recipe data model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }

ef1af326393d2c6b141376ff4cbde30bfcd6c31f
William Liu <superliuxz@gmail.com>
2019-07-01 16:29:59 -0700

    change New Recipe button from [routeLink] to (click)

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {Recipe} from '../recipe.model';
 import {RecipeService} from '../recipe.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,11 +10,23 @@ import {RecipeService} from '../recipe.service';
 export class RecipeListComponent implements OnInit {
   recipes: Recipe[];
 
-  constructor(private recipeSvc: RecipeService) {
+  constructor(private recipeSvc: RecipeService,
+              private route: ActivatedRoute,
+              private router: Router) {
   }
 
   ngOnInit() {
     this.recipes = this.recipeSvc.getRecipes();
+  }
+
+  onNewRecipe() {
+    this.router.navigate(['new'], {relativeTo: this.route}).then(
+      (navSuccess: boolean) => {
+        if (!navSuccess) {
+          console.log('failed to navigate to /recipes/new');
+        }
+      }
+    );
   }
 
 }

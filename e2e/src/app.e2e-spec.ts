13cd9eacb8308d55387c3ee75370cc9722fb53a9
William Liu <superliuxz@gmail.com>
2019-07-18 21:13:20 -0700

    rename project meta data

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to course-project!');
+    expect(page.getTitleText()).toEqual('Welcome to recipe-book!');
   });
 
   afterEach(async () => {
